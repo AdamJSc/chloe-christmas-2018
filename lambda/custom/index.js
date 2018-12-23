a182cfae31aac11e711b5f16338362ae8bd328f0
Adam Scholey <hello@adamjs.net>
2018-12-23 16:13:56 +0000

    Remove demo intent handler

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -118,23 +118,6 @@ const GuessIntentHandler = {
 	}
 };
 
-const HelloWorldIntentHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-  },
-  handle(handlerInput) {
-  	session.load(handlerInput);
-
-    const speechText = 'Hello World!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
-};
-
 const HelpIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -208,7 +191,6 @@ exports.handler = skillBuilder
     PositiveIntentHandler,
     NextClueIntentHandler,
     GuessIntentHandler,
-    HelloWorldIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
